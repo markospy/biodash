fda18b2cb9bf623a925bc8bdd641fa0acea7544a
Marcos Avila <marco.developer@outlook.com>
2023-11-27 17:18:42 -0500

    Add schemas/schemas.py

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -1,3 +1,28 @@
+from datetime import datetime, date
+
 from pydantic import BaseModel
 
-user
+from models.models import Gender
+
+
+class PatientOut(BaseModel):
+    first_name: str
+    second_name: str | None = None
+    last_name: str | None = None
+    age: int | None = None
+    gender: Gender | None = None
+    height: int | None = None
+    weight: float | None = None
+
+
+class PatientIn(PatientOut):
+    password: str
+
+
+class BloodPressureSchema(BaseModel):
+    systolic: int
+    diastolic: int
+    heart_date: int | None = None
+    date: date | None = None
+    hour: datetime
+    patient_id: int
