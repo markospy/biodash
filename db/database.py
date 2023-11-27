69c6b6787e9000f145f07924f80d0bae33552f5e
Marcos Avila <marco.developer@outlook.com>
2023-11-27 16:50:05 -0500

    Creation of db/database.py

---
+++ db/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+DATABASE_URL = "sqlite://base.db"
+engine = create_engine(DATABASE_URL, echo=True)
+
+session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def create_tables():
+    Base.metadata.create_all(engine)
