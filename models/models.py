d8fcf34cf4be9d34ddf284f918035eccdc925b41
Marcos Avila <marco.developer@outlook.com>
2023-11-27 19:57:19 -0500

    Add model User in models/models.py

--- models/models.py
+++ models/models.py
@@ -13,11 +13,20 @@ class Gender(enum.Enum):
     female = 2
 
 
+class User(Base):
+    __tablename__ = "user"
+
+    usename: Mapped[str] = mapped_column(primary_key=True)
+    password: Mapped[str]
+    patient_relarionship: Mapped[list["Patient"]] = relationship(
+        back_populates="username_relarionship", cascade="all, delete"
+    )
+
+
 class Patient(Base):
     __tablename__ = "patients"
 
     id_patient: Mapped[int] = mapped_column(primary_key=True)
-    password: Mapped[str]
     first_name: Mapped[str]
     second_name: Mapped[str | None]
     last_name: Mapped[str | None]
@@ -25,6 +34,10 @@ class Patient(Base):
     gender: Mapped[Gender | None] = mapped_column(Enum(Gender))
     height: Mapped[int | None]  # altura en centímetros
     weight: Mapped[float | None]  # peso en kilogramos
+    username: Mapped["User"] = mapped_column(ForeignKey("user.username"))
+    username_relarionship: Mapped["User"] = relationship(
+        back_populates="patient_relarionship"
+    )
     measure: Mapped[list["BloodPressure"]] = relationship(
         back_populates="patient", cascade="all, delete"
     )
@@ -41,4 +54,5 @@ class BloodPressure(Base):
         DateTime(timezone=True), server_default=func.now(), primary_key=True
     )
     patient_id: Mapped[int] = mapped_column(ForeignKey("patients.id"), primary_key=True)
+
     patient: Mapped["Patient"] = relationship(back_populates="measure")
