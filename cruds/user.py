2ea137a2dba6c9b1845eab2195bf96c8237220a8
Marcos Avila <marco.developer@outlook.com>
2023-11-27 21:20:33 -0500

    Add cruds/user.py

---
+++ cruds/user.py
@@ -0,0 +1,19 @@
+from fastapi import status
+from fastapi.exceptions import HTTPException
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from models.models import User
+from schemas.schemas import UserSchema
+
+
+# Register user
+def register(db: Session, user: UserSchema):
+    stmt = select(User).where(User.usename == UserSchema.username)
+    result = db.scalars(stmt).one_or_none()
+    if result:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail="The user already exists"
+        )
+    db.add(User(user.model_dump()))
