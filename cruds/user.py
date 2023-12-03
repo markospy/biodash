5589841e2c70a9b5e5af93e9842b184914e4d9c7
Marcos Avila <marco.developer@outlook.com>
2023-12-03 01:47:34 -0500

    Changes in register function

--- cruds/user.py
+++ cruds/user.py
@@ -4,18 +4,23 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from models.models import User
-from schemas.schemas import UserSchema
+from schemas.schemas import UserIn, UserInDB
+from security.security import get_password_hash
 
 
 # Register user
-def register(db: Session, user: UserSchema):
-    stmt = select(User).where(User.usename == UserSchema.username)
+def register(db: Session, user: UserIn):
+    stmt = select(User).where(User.usename == UserIn.username)
     result = db.scalars(stmt).one_or_none()
     if result:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT, detail="The user already exists"
         )
-    user.password = get_password_hash(
-        user.password
-    )  # Encrypt the password entered by the user
-    db.add(User(user.model_dump()))
+
+    # Encrypt the password entered by the user
+    hashed_password = get_password_hash(user.model_dump["password"])
+    user_bd = UserInDB(
+        username=user.model_dump()["username"], hashed_password=hashed_password
+    )
+
+    db.add(User(**user_bd.model_dump()))
