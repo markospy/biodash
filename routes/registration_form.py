24250a1d00fd656b89ae1e322a0acfabe2f9d617
Marcos Avila <marco.developer@outlook.com>
2023-12-03 02:12:39 -0500

    Modify

--- routes/registration_form.py
+++ routes/registration_form.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from dependencies.dependencies import get_db
 from cruds.user import register
-from schemas.schemas import UserSchema
+from schemas.schemas import UserIn
 
 router = APIRouter(prefix="register", tags=["Registration"])
 
@@ -11,4 +11,4 @@ router = APIRouter(prefix="register", tags=["Registration"])
 @router.post()
 def register_user(db: Session = Depends(get_db)):
     """User registration"""
-    register(db, UserSchema)
+    register(db, UserIn)
