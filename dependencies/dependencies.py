b3b93b88cce58508495cda346d741fa26b602a8b
Marcos Avila <marco.developer@outlook.com>
2023-12-03 02:08:06 -0500

    Change SessionLocal occurrences to session_local

--- dependencies/dependencies.py
+++ dependencies/dependencies.py
@@ -1,9 +1,9 @@
-from db.database import SessionLocal
+from db.database import session_local
 
 
 # Dependency database
 def get_db():
-    db = SessionLocal()
+    db = session_local()
     try:
         yield db
     finally:
