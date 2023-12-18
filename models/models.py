1c8871c35c8187eddeab8371fc4d57f63c6e0ba3
Marcos Avila <marco.developer@outlook.com>
2023-12-18 11:39:55 -0500

    Change the basedate to mysql

--- models/models.py
+++ models/models.py
@@ -1,6 +1,6 @@
 import enum
 
-from sqlalchemy import Enum, create_engine, ForeignKey, func, DateTime
+from sqlalchemy import Enum, create_engine, ForeignKey, func, DateTime, String
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
@@ -20,10 +20,10 @@ class Gender(enum.Enum):
 
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "users"
 
-    username: Mapped[str] = mapped_column(primary_key=True)
-    hashed_password: Mapped[str]
+    username: Mapped[str] = mapped_column(String(30), primary_key=True)
+    hashed_password: Mapped[str] = mapped_column(String(255))
     patient_relarionship: Mapped[list["Patient"]] = relationship(
         back_populates="username_relarionship", cascade="all, delete"
     )
@@ -32,18 +32,18 @@ class User(Base):
 class Patient(Base):
     __tablename__ = "patients"
 
-    id: Mapped[str] = mapped_column(primary_key=True)
-    first_name: Mapped[str]
-    second_name: Mapped[str | None]
-    last_name: Mapped[str | None]
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(30))
+    second_name: Mapped[str | None] = mapped_column(String(30))
+    last_name: Mapped[str | None] = mapped_column(String(30))
     age: Mapped[int | None]
     gender: Mapped[Gender | None] = mapped_column(Enum(Gender))
-    height: Mapped[int | None]  # altura en centímetros
-    weight: Mapped[float | None]  # peso en kilogramos
-    username: Mapped["User"] = mapped_column(ForeignKey("user.username"))
-    username_relarionship: Mapped["User"] = relationship(
-        back_populates="patient_relarionship"
+    height: Mapped[int | None]
+    weight: Mapped[float | None]
+    username: Mapped["User"] = mapped_column(
+        ForeignKey("users.username", ondelete="CASCADE")
     )
+    username_relarionship = relationship("User", back_populates="patient_relarionship")
     measure: Mapped[list["BloodPressure"]] = relationship(
         back_populates="patient", cascade="all, delete"
     )
@@ -55,17 +55,20 @@ class BloodPressure(Base):
     systolic: Mapped[int] = mapped_column(default=120)
     diastolic: Mapped[int] = mapped_column(default=80)
     heart_rate: Mapped[int | None] = mapped_column(default=None)
-    date = mapped_column(DateTime(timezone=True), server_default=func.now())
-    hour = mapped_column(
-        DateTime(timezone=True), server_default=func.now(), primary_key=True
+    date = mapped_column(DateTime(timezone=True), primary_key=True)
+    patient_id: Mapped[int] = mapped_column(
+        ForeignKey("patients.id", ondelete="CASCADE"), primary_key=True
     )
-    patient_id: Mapped[int] = mapped_column(ForeignKey("patients.id"), primary_key=True)
-
-    patient: Mapped["Patient"] = relationship(back_populates="measure")
+    patient = relationship("Patient", back_populates="measure")
 
 
-DATABASE_URL = "sqlite:///db/base.db"
-engine = create_engine(DATABASE_URL, echo=True)
+# Motor sqlite
+# engine = create_engine("sqlite:///db/base.db", echo=True)
+
+# Motor mysql
+engine = create_engine(
+    "mysql+pymysql://myuser:mypassword@localhost/mydb?charset=utf8mb4"
+)
 
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
