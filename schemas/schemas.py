14ae873a698df2159b8d64e5f859576598149593
Marcos Avila <marco.developer@outlook.com>
2023-12-18 11:34:15 -0500

    Add AnalizeBloodPressure schema

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from pydantic import BaseModel
 
-# from models.models import Gender, User
+from models.models import Gender, User
 
 
 class UserSchema(BaseModel):
@@ -17,22 +17,26 @@ class UserInDB(UserSchema):
     hashed_password: str
 
 
-# class PatientSchema(BaseModel):
-#     patient_id: str
-#     first_name: str
-#     second_name: str | None = None
-#     last_name: str | None = None
-#     age: int | None = None
-#     gender: Gender | None = None
-#     height: int | None = None
-#     weight: float | None = None
-#     username: User
+class PatientSchema(BaseModel):
+    first_name: str
+    second_name: str | None = None
+    last_name: str | None = None
+    age: int | None = None
+    gender: Gender | None = None
+    height: int | None = None
+    weight: float | None = None
+    username: str
 
 
-# class BloodPressureSchema(BaseModel):
-#     systolic: int
-#     diastolic: int
-#     heart_date: int | None = None
-#     date: date | None = None
-#     hour: datetime
-#     patient_id: int
+class BloodPressureSchema(BaseModel):
+    systolic: int
+    diastolic: int
+    heart_rate: int | None = None
+    date: datetime
+    patient_id: int
+
+
+class AnalizeBloodPressure(BaseModel):
+    systolic: float | None = None
+    diastolic: float | None = None
+    heart_rate: float | None = None
