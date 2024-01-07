e39269be1d0ea8a4001503a5303b53245901dc10
Marcos Avila <marco.developer@outlook.com>
2024-01-07 04:40:51 -0500

    Add routes in charge of managing the verification code for the email

---
+++ routes/email.py
@@ -0,0 +1,50 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, status
+from fastapi.exceptions import HTTPException
+from fastapi.responses import JSONResponse
+from sqlalchemy import select, update
+from sqlalchemy.orm import Session
+
+from dependencies.dependencies import get_db
+from models.models import Doctor, Email
+from routes.jwt_oauth_doctor import get_current_user
+from sendemail.sendemail import send_email
+
+router = APIRouter(tags=["Email verification"])
+
+
+@router.post("/email_verification")
+def send_code(
+    code: int,
+    current_doctor: Annotated[Doctor, Depends(get_current_user)],
+    db: Session = Depends(get_db),
+):
+    """**Verify the email of the authenticated doctor with the code sent to the email**"""
+    stmt = select(Email).where(Email.doctor_id == current_doctor.doctor_id)
+    email_bd = db.scalars(stmt).first()
+    if email_bd.email_verify:
+        return JSONResponse(content={"message": "The email is already verified"})
+    if code != email_bd.code:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={"error": "The code is invalid"})
+    stmt = update(Email).where(Email.doctor_id == current_doctor.doctor_id).values(email_verify=True)
+    db.execute(stmt)
+    db.commit()
+    return JSONResponse(content={"message": "The email is already verified"})
+
+
+@router.get("/email_verification")
+def request_code(
+    current_doctor: Annotated[Doctor, Depends(get_current_user)],
+    db: Session = Depends(get_db),
+):
+    """**Send a new validation email to the doctor's registered email**"""
+    stmt = select(Email).where(Email.doctor_id == current_doctor.doctor_id)
+    email_bd = db.scalars(stmt).first()
+    if email_bd.email_verify:
+        return JSONResponse(content={"message": "The email is already verified"})
+    code = send_email(current_doctor.first_name, email_bd.email_address)
+    stmt = update(Email).where(Email.doctor_id == current_doctor.doctor_id).values(code=code)
+    db.execute(stmt)
+    db.commit()
+    return JSONResponse(content={"message": "A new verification code has been sent to your inbox"})
