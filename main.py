c544274cbaeaf50d059603b8f15f9aeaeaade7f9
Marcos Avila <marco.developer@outlook.com>
2024-01-07 04:44:59 -0500

    Only doctor routes, patient emails

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from routes import analize, blood_pressure, jwt_oauth_user, patients, user
+from routes import doctors, jwt_oauth_doctor, patients, email
 from models.models import create_tables
 
 
@@ -19,15 +19,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-app.include_router(user.router)
+app.include_router(doctors.router)
 app.include_router(patients.router)
-app.include_router(blood_pressure.router)
-app.include_router(analize.router)
-app.include_router(jwt_oauth_user.router)
+# app.include_router(blood_pressure.router)
+# app.include_router(analize.router)
+app.include_router(email.router)
+app.include_router(jwt_oauth_doctor.router)
 
 
 @app.get("/")
 async def root():
-    return {
-        "message": "This aplication can be used to keep track of blood pressure values"
-    }
+    return {"message": "This aplication can be used to keep track of blood pressure values"}
