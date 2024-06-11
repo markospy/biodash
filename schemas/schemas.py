2d840499ae09a2abc5ace0cccf1a03eb0eb45dde
Marcos Avila <marcosantonioavilamorales@gmail.com>
2024-06-11 12:18:35 -0400

    perf: change and creation of models

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -1,3 +1,4 @@
+from typing import List
 from pydantic import BaseModel, EmailStr, validator, Field
 
 from datetime import datetime
@@ -5,34 +6,21 @@ from datetime import datetime
 from models.enumerations import Gender, Scholing
 
 
-class EmailSchema(BaseModel):
-    email_address: EmailStr | None = None
-    email_verify: bool = False
-    code: int | None = None
-    doctor_id: str
-
-
-class AddressSchema(BaseModel):
-    address_id: int
-    address: dict = Field(examples=[{"Provincia": "Ciefuegos", "Barrio": "Pastorita"}], description="Recibe un objeto")
-
-
 class Doctor(BaseModel):
-    id: str = Field(
-        examples=["3210"],
-        description="El formato de este id es distinto en cada país, puede ser un número o una cadena de letras y dígitos",
-    )
+    id: str = Field(examples=["3210"], description="El identificador del doctor")
     first_name: str = Field(examples=["Marcos Antonio"])
     last_name: str = Field(examples=["Avila Morales"])
     specialty: str = Field(examples=["Médico General"])
     email_address: str | None = Field(
-        examples=["markos@email.com"], pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", default=None
+        examples=["doctor_mail@yahoo.com"], pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", default=None
     )
 
 
 class DoctorOut(Doctor):
-    email_verify: bool
-    photo: str | None = Field(default=None, examples=["http://biodash.com/photos/3210.png"])
+    email_verify: bool = Field(examples=[True], description="Si el correo ha sido verificado")
+    photo: str | None = Field(
+        default=None, examples=["http://biodash.com/photos/3210.png"], description="URL con el avatar del doctor"
+    )
 
 
 class DoctorIn(Doctor):
@@ -40,11 +28,7 @@ class DoctorIn(Doctor):
 
 
 class DoctorUp(DoctorIn):
-    id: str | None = Field(
-        examples=["3210"],
-        description="El formato de este id es distinto en cada país, puede ser un número o una cadena de letras y dígitos",
-        default=None,
-    )
+    id: str | None = Field(examples=["3210"], description="El identificador del doctor", default=None)
     first_name: str | None = Field(examples=["Marcos Antonio"], default=None)
     last_name: str | None = Field(examples=["Avila Morales"], default=None)
     specialty: str | None = Field(examples=["Médico General"], default=None)
@@ -61,6 +45,18 @@ class DoctorPhoto(Doctor):
     portrait: str | None = None
 
 
+class EmailSchema(BaseModel):
+    email_address: EmailStr | None = None
+    email_verify: bool = False
+    code: int | None = None
+    doctor_id: str
+
+
+class AddressSchema(BaseModel):
+    address_id: int
+    address: dict = Field(examples=[{"Provincia": "Ciefuegos", "Barrio": "Pastorita"}], description="Recibe un objeto")
+
+
 class PatientSchema(BaseModel):
     id: str = Field(
         examples=["39231"],
@@ -68,7 +64,7 @@ class PatientSchema(BaseModel):
     )
     first_name: str = Field(examples=["Javier"])
     last_name: str = Field(examples=["Hernandez Lao"])
-    birth_date: datetime | None = Field(default=None, examples=["04-29-1979"])
+    birth_date: datetime | None = Field(default=None, examples=["2000-01-01T12:00:00"])
     gender: Gender | None = Field(
         default=None, examples=["female", "male"], description="Solo puede recibir alguno de los valores de ejemplo"
     )
@@ -76,10 +72,10 @@ class PatientSchema(BaseModel):
     weight: float | None = Field(default=None, examples=[80])
     scholing: Scholing | None = Field(
         default=None,
-        examples=["primary", "secunadry", "pre_university", "university", "middle_technical"],
+        examples=["primary", "secunadry", "pre university", "university", "middle technical"],
         description="Solo puede recibir alguno de los valores de ejemplo",
     )
-    employee: bool | None = Field(default=None, examples=["Mechanic"])
+    employee: bool | None = Field(default=None, examples=[True])
     married: bool | None = Field(default=None, examples=[True])
     address: dict | None = Field(
         default=None, examples=[{"Provincia": "Ciefuegos", "Barrio": "Pastorita"}], description="Recibe un objeto"
@@ -98,31 +94,35 @@ class PatientUp(PatientSchema):
 
 
 class DoctorPatient(BaseModel):
-    doctor_id: str = Field(examples=[321])
-    patient_id: str = Field(examples=[39231])
+    doctor_id: str
+    patient_id: str
 
 
-class CardiovascularParameterOut(BaseModel):
-    systolic: int | None = Field(default=None, examples=[120])
-    diastolic: int | None = Field(default=None, examples=[80])
-    heart_rate: int | None = Field(default=None, examples=[72])
-    date: datetime | None = Field(default=None, examples=["04-29-2024"])
+class CardiovascularParameterIn(BaseModel):
+    id: int
+    systolic: int
+    diastolic: int
+    heart_rate: int
+    date: datetime
 
 
-class CardiovascularParameter(CardiovascularParameterOut):
-    systolic: int = 120
-    diastolic: int = 80
-    patient_id: str = Field(examples=[39231])
+class CardiovascularParameterOut(CardiovascularParameterIn):
+    patient_id: str
 
+
+class BloodSugarLevel(BaseModel):
+    patient_id: str | None = None
+    date: datetime | None = None
+    value: float
+
+
+class BloodSugarLevelUpdate(BaseModel):
+    date: datetime | None = None
+    value: float | None = None
 
 class BloodSugarLevelOut(BaseModel):
-    date: datetime | None = Field(default=None, examples=["04-29-2024"])
-    value: float | None = Field(default=None, examples=[5.2])
-
-
-class BloodSugarLevelIn(BloodSugarLevelOut):
-    patient_id: str = Field(examples=[39231])
-
+    patient_id: int
+    measures: List[BloodSugarLevelUpdate]
 
 class Analize(BaseModel):
     minimum: float
