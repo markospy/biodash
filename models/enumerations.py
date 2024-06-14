13d1c815f03244923570aefa6a50ea43ef8af6ff
Marcos Avila <marcosantonioavilamorales@gmail.com>
2024-06-14 16:01:23 -0400

    feat: create SortBy enumeration

--- models/enumerations.py
+++ models/enumerations.py
@@ -22,6 +22,17 @@ class SortBy(str, Enum):
     gender = "gender"
     height = "height"
     weight = "weight"
+    employee = "employee"
+    married = "married"
+
+
+class FilterBy(str, Enum):
+    first_name = "first name"
+    last_name = "last name"
+    birth_date = "birth date"
+    gender = "gender"
+    height = "height"
+    weight = "weight"
     scholing = "scholing"
     employee = "employee"
     married = "married"
