3a37227c24fa620b55c1e1e31062af90273aa115
Marcos Avila <marcosantonioavilamorales@gmail.com>
2024-06-16 12:33:45 -0400

    feat: add warning route

--- routes/analize_blood_sugar.py
+++ routes/analize_blood_sugar.py
@@ -1,18 +1,22 @@
+from datetime import datetime, timedelta
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from dependencies.dependencies import get_db
-from models.models import BloodSugarLevel, Doctor
-from schemas.schemas import Analize
+from models.models import BloodSugarLevel, Doctor, Patient
+from schemas.schemas import Analize, WarningBloodSugar
 from routes.oauth import get_current_user
 from routes.calc.calculation import make_analize
+from models.exceptions import exception_if_not_exists
 
 router = APIRouter(prefix="/analize", tags=["Analize"])
 
 
-@router.get("/blood-sugar", response_model=Analize)
+@router.get("/blood_sugar", response_model=Analize)
 def analize(
     current_doctor: Annotated[Doctor, Depends(get_current_user)],
     patient_id: str,
@@ -24,3 +28,38 @@ def analize(
     """
     minimum, maximum, mean = make_analize(patient_id, db, BloodSugarLevel.value, BloodSugarLevel)
     return Analize(minimum=minimum, maximum=maximum, mean=mean)
+
+
+@router.get("/warning_blood_sugar", response_model=list[WarningBloodSugar])
+def get_warning_patients(
+    current_doctor: Annotated[Doctor, Depends(get_current_user)],
+    value: float | None = 6.1,
+    day: int | None = 1,
+    hours: int | None = 0,
+    db: Session = Depends(get_db),
+):
+    stmt = select(BloodSugarLevel).where(
+        BloodSugarLevel.value >= value,
+        BloodSugarLevel.date >= (datetime.now() - timedelta(days=day, hours=hours)),
+        BloodSugarLevel.doctor_id == current_doctor.id,
+    )
+
+    patients_measures = db.scalars(stmt).all()
+
+    if not patients_measures:
+        return JSONResponse("No hay pacientes con problemas", status_code=404)
+    patient_dict = {}
+    patient_list = []
+    for measures in patients_measures:
+        stmt = select(Patient).where(Patient.id == measures.patient_id)
+        patient = db.scalars(stmt).first()
+        exception_if_not_exists(patient, "Paciente no encontrado")
+
+        patient_dict["patient_id"] = measures.patient_id
+        patient_dict["value"] = measures.value
+        patient_dict["date"] = measures.date
+        patient_dict["first_name"] = patient.first_name
+        patient_dict["last_name"] = patient.last_name
+        patient_list.append(WarningBloodSugar(**patient_dict))
+
+    return patient_list
